7b9499878579f966da9b84acfb60dc2250318c3f
IgorGalimski <igorg@playtika.com>
2020-07-18 13:04:13 +0300

    Add load and instantiate

---
+++ Assets/Scripts/InstantiateScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class InstantiateScript : MonoBehaviour
+{
+    [SerializeField] 
+    private AssetReference _cubeAssetReference;
+
+    public void OnLoadClick()
+    {
+        var loadAssetOperation = _cubeAssetReference.LoadAssetAsync<GameObject>();
+        loadAssetOperation.Completed += OnLoadComplete;
+    }
+
+    private void OnLoadComplete(AsyncOperationHandle<GameObject> asyncOperationHandle)
+    {
+        Debug.Log($"Load complete with status{asyncOperationHandle.Status} IsDone:{asyncOperationHandle.IsDone}");
+        
+        var go = asyncOperationHandle.Result;
+
+        Instantiate(go);
+    }
+
+    public void OnInstantiateClick()
+    {
+        var instantiateOperation = _cubeAssetReference.InstantiateAsync();
+        instantiateOperation.Completed += OnInstantiateComplete;
+    }
+
+    private void OnInstantiateComplete(AsyncOperationHandle<GameObject> asyncOperationHandle)
+    {
+        Debug.Log($"Instantiate complete with status{asyncOperationHandle.Status} IsDone:{asyncOperationHandle.IsDone}");
+    }
+}
